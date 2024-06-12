0bc093c2896c6820280801a7792d38b7f0be2f02
maverickPrakash <Prakashtheryon@gmail.com>
2024-06-11 23:09:44 -0400

    django

--- core/home/views.py
+++ core/home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
 
 # Create your views here.
+peoples = [{"name":"Ravi Kumar", "age":18},{"name":"Ivan Kumar", "age":8},{"name":"Prakash Kumar", "age":18} ]
+def home(request):
+    return render(request, "index.html", context={"people":peoples})
+def sucess(request):
+    return HttpResponse("This page is render sucessfully")
